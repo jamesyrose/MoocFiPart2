bedd3082fd8636a25276b6fe711e42cf4d309dc3
James Rose <jyrose@verlet.io>
2020-09-23 16:14:04 -0700

    Finished

--- src/Part9/Person.java
+++ src/Part9/Person.java
@@ -7,10 +7,36 @@ public class Person {
 
     protected String name;
     protected String address;
+    protected String lastName;
+    protected int birthYear;
     
     public Person(String name, String address) {
     	this.name = name; 
     	this.address = address;
+    }
+    
+    
+    public Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+    
+    public Person(String firstName, String lastName, int birthYear){
+    	this.name= firstName; 
+    	this.lastName = lastName;
+    	this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getBirthYear() {
+        return this.birthYear;
+    }
+    
+    public String getLastName() {
+    	return this.lastName;
     }
 
     @Override
